a04758b1a6d419e5f6c8f7afb69d1cb6ee11edcf
CJai <p.cjai@yahoo.com>
2019-01-16 22:55:44 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: CJai Payne
+ * Date: 2019/16/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("CJai");
+
+  return 0;
+}
